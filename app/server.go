b5c993467bc09d674f89e4ca142a19b1c9800364
Amjad Jibon <amjad.jibon@gmail.com>
2024-03-09 17:26:54 +0800

    passed 3rd stage

--- app/server.go
+++ app/server.go
@@ -1,30 +1,77 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net"
-	"os"
+	"strings"
 )
 
-func main() {
-	fmt.Println("Logs from your program will appear here!")
+type Request struct {
+	Method   string
+	URI      string
+	Protocol string
+}
 
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
-	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
-		os.Exit(1)
-	}
+func parseRequest(req []byte) Request {
+	request := Request{}
 
-	conn, err := l.Accept()
+	// split the request into lines
+	lines := strings.Split(string(req), "\r\n")
+
+	// split the first line
+	firstLine := strings.Split(lines[0], " ")
+
+	// parse the first line
+	request.Method = firstLine[0]
+	request.URI = firstLine[1]
+	request.Protocol = firstLine[2]
+
+	return request
+}
+
+func handleConnection(conn net.Conn) {
 	defer func() {
 		_ = conn.Close()
 	}()
 
+	buf := make([]byte, 1024)
+	req, err := conn.Read(buf)
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+		log.Fatal(err)
 	}
 
-	fmt.Println("Connection accepted")
-	_, _ = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	// parse the request
+	request := parseRequest(buf[:req])
+
+	// handle the request based on the method and URI
+	if request.Method != "GET" {
+		_, _ = conn.Write([]byte("HTTP/1.1 405 Method Not Allowed\r\n\r\n"))
+		return
+	}
+
+	if request.URI == "/" || request.URI == "/index.html" {
+		_, _ = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+		return
+	} else {
+		_, _ = conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+		return
+	}
+}
+
+func main() {
+	var addr = "localhost:4221"
+	log.Printf("Server started on: %s", addr)
+
+	listen, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		conn, err := listen.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		handleConnection(conn)
+	}
 }
