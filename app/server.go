df76af7f6389f5efd2f3cdd1a6f1c5431deb9c53
Amjad Jibon <amjad.jibon@gmail.com>
2024-03-10 01:26:23 +0800

    pass 8th stage

--- app/server.go
+++ app/server.go
@@ -14,6 +14,7 @@ type Request struct {
 	Headers  map[string]string
 	URI      string
 	Protocol string
+	Body     []byte
 }
 
 func parseRequest(req []byte) Request {
@@ -30,13 +31,14 @@ func parseRequest(req []byte) Request {
 	request.URI = firstLine[1]
 	request.Protocol = firstLine[2]
 
-	// parse the headers
+	// parse the headers and body
 	request.Headers = make(map[string]string)
-	for _, line := range lines[1:] {
-		if line == "" {
-			break // end of headers section
+	for i := 1; i < len(lines); i++ {
+		if lines[i] == "" {
+			request.Body = []byte(strings.Join(lines[i+1:], "\r\n"))
+			break
 		}
-		header := strings.Split(line, ": ")
+		header := strings.Split(lines[i], ": ")
 		request.Headers[header[0]] = header[1]
 	}
 
@@ -57,12 +59,6 @@ func handleConnection(conn net.Conn, directory string) {
 	// parse the request
 	request := parseRequest(buf[:req])
 
-	// handle the request based on the method and URI
-	if request.Method != "GET" {
-		_, _ = conn.Write([]byte("HTTP/1.1 405 Method Not Allowed\r\n\r\n"))
-		return
-	}
-
 	if request.URI == "/" || request.URI == "/index.html" {
 		_, _ = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 		return
@@ -75,7 +71,7 @@ func handleConnection(conn net.Conn, directory string) {
 		response := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",
 			len(userAgentHeader), userAgentHeader)
 		_, _ = conn.Write([]byte(response))
-	} else if strings.HasPrefix(request.URI, "/files/") {
+	} else if strings.HasPrefix(request.URI, "/files/") && request.Method == "GET" {
 		filePath := strings.TrimPrefix(request.URI, "/files/")
 		file, err := os.Open(directory + "/" + filePath)
 		if err != nil {
@@ -96,6 +92,26 @@ func handleConnection(conn net.Conn, directory string) {
 			len(fileBody), fileBody,
 		)
 		_, _ = conn.Write([]byte(response))
+	} else if strings.HasPrefix(request.URI, "/files/") && request.Method == "POST" {
+		filePath := strings.TrimPrefix(request.URI, "/files/")
+		file, err := os.Create(directory + "/" + filePath)
+		if err != nil {
+			_, _ = conn.Write([]byte("HTTP/1.1 500 Internal Server Error\r\n\r\n"))
+			return
+		}
+
+		// write body to file
+		_, err = file.Write(request.Body)
+		if err != nil {
+			_, _ = conn.Write([]byte("HTTP/1.1 500 Internal Server Error\r\n\r\n"))
+			return
+		}
+
+		response := fmt.Sprintf("HTTP/1.1 201 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",
+			len("File created"), "File created")
+		_, _ = conn.Write([]byte(response))
+		return
+
 	} else {
 		_, _ = conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 		return
